e7a47834330b4ffc27c7e009fceb2627e83e2e20
hossein <hossein.h.a770227@gmail.com>
2025-04-30 09:49:56 +0330

    add test for urls.py app website

---
+++ website/tests/test_urls.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+
+from website.views import about_view, contact_view
+
+
+class TestUrls(TestCase):
+    def test_about_view_url(self):
+        url = reverse("website:about-us")
+        self.assertEqual(resolve(url).func, about_view)
+
+    def test_contact_view_url(self):
+        url = reverse("website:contact")
+        self.assertEqual(resolve(url).func, contact_view)
