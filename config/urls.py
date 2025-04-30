dac10e8c52f8d180cc334c22c1406f33c0f545ce
hossein <hossein.h.a770227@gmail.com>
2025-04-30 21:15:20 +0330

    add urls.py for dashboard app

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('products/', include('shop.urls')),
     path('', include('website.urls')),
+    path('dashboard/', include('dashboard.urls')),
 
     # rosetta url for translate
     path('rosetta/', include('rosetta.urls')),
