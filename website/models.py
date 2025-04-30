b95b6d5bf62c8cfb4d604417eb9e523c847cdfa7
hossein <hossein.h.a770227@gmail.com>
2025-04-30 10:17:28 +0330

    add contact models

--- website/models.py
+++ website/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+class  Contact(models.Model):
+    email = models.EmailField(_("email"), max_length=254)
+    message_subject = models.CharField(_("message subject"), max_length=100)
+    message = models.TextField(_("message"))
+    date_time = models.DateTimeField(_("date time"), auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.email} : {self.message_subject}' 
