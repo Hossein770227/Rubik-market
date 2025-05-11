bec7130932f620eee55d693957ddba36d5110136
hossein <hossein.h.a770227@gmail.com>
2025-05-11 16:13:38 +0330

    add test for urls.py app website

--- website/tests/test_urls.py
+++ website/tests/test_urls.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 
-from website.views import about_view, contact_view
+from website.views import about_view, contact_view, question_view, search
 
 
 class TestUrls(TestCase):
@@ -12,3 +12,11 @@ class TestUrls(TestCase):
     def test_contact_view_url(self):
         url = reverse("website:contact")
         self.assertEqual(resolve(url).func, contact_view)
+    
+    def test_question_view_url(self):
+        url = reverse('website:question')
+        self.assertEqual(resolve(url).func, question_view)
+    
+    def test_search_view_url(self):
+        url = reverse("website:search")
+        self.assertEqual(resolve(url).func, search)
